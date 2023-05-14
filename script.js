2c76a5d688e87498636321e0c28ab356778a9c72
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-05-14 17:36:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+function updateText() {
+	return new Promise(resolve=>{
+	    setTimeout(resolve, 1000, "Hello, world!");
+	})
+}
+const promise = updateText();
+promise.then(data=>{
+	output.innerText=data;
+})
